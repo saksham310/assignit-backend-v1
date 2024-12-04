91badfc6ad2761e6c65af1b834c5527b47312cce
saksham.s <sakshams982@gmail.com>
2024-12-04 19:20:05 +0545

    backend setup

---
+++ src/server.js
@@ -0,0 +1,9 @@
+import express from 'express';
+import 'dotenv/config';
+const app = express();
+const PORT = process.env.PORT || 3000;
+
+app.use(express.json());
+app.listen(PORT, () => {
+    console.log(`Server is running on port ${PORT}`);
+});
