6703d4142b628b582ea3982f14435c873684a5d3
saksham.s <sakshams982@gmail.com>
2024-12-12 10:40:38 +0545

    #86eqy2n22: Added cors and updated the package

--- src/server.js
+++ src/server.js
@@ -3,6 +3,11 @@ import 'dotenv/config';
 import authRoute from './routes/authRoutes.js';
 const app = express();
 const PORT = process.env.PORT || 3000;
+import cors from 'cors';
+app.use(cors({
+    origin: 'http://localhost:5173', // Allow requests from your frontend
+    methods: ['GET', 'POST', 'PUT', 'DELETE'], // Allowed HTTP methods
+}));
 
 app.use(express.json());
 app.use('/api/auth', authRoute)
