fb5ebec83c43b9c0321b1f963f770ef42923ed02
saksham.s <sakshams982@gmail.com>
2024-12-28 21:52:45 +0545

    Added a delete api to delete the workspace and its associated users, projects with authorization check

--- src/controllers/workspaceController.js
+++ src/controllers/workspaceController.js
@@ -27,7 +27,6 @@ const newWorkspace=await prisma.workspace.create({
 
 export const getWorkspace=async (req,res)=>{
     try{
-console.log(req.headers);
     const workspaces=await prisma.workspace.findMany({
         where:{
             users:{
@@ -53,5 +52,60 @@ console.log(req.headers);
     }catch(e){
         console.log(e);
         return  res.status(503).json({message:'Failed to fetch the Workspaces'});
+    }
+}
+
+export const deleteWorkspace=async (req,res)=>{
+    try{
+        const workspaceId =parseInt(req.params.workspaceId);
+        const workspace=await prisma.workspace.findUnique(
+            {
+                where:{
+                    id:workspaceId,
+                }
+            });
+        if(!workspace){
+            return res.status(404).json({message:'Workspace not found'});
+        }
+        const user=await prisma.workspace_User.findFirst({
+            where:{
+                workspace_id:workspaceId,
+                user_id:req.userId,
+                role:"Owner"
+            }
+        });
+        if(!user){
+            return res.status(404).json({message:'You are not authorized to delete this workspace'});
+        }
+        await prisma.workspace_User.deleteMany({
+            where:{
+                workspace_id:workspaceId,
+            }
+        })
+        await prisma.project_User.deleteMany(
+            {
+                where:{
+                    project:{
+                        workspace_id:workspaceId,
+                    }
+                }
+            }
+        );
+        await prisma.project.deleteMany({
+            where:{
+                workspace_id:workspaceId,
+            }
+        });
+
+        await prisma.workspace.delete({
+            where:{
+                id:workspaceId,
+            }
+        });
+
+        return res.status(200).json({message:'Workspace deleted successfully'});
+    }catch (e){
+        console.log(e);
+        return  res.status(500).json({message:'Failed to delete workspace'});
     }
 }
--- src/routes/workspaceRoutes.js
+++ src/routes/workspaceRoutes.js
@@ -1,10 +1,10 @@
 import express from "express";
-import {createWorkspace, getWorkspace} from "../controllers/workspaceController.js";
+import {createWorkspace, deleteWorkspace, getWorkspace} from "../controllers/workspaceController.js";
 import authMiddleware from "../middleware/authMiddleware.js";
 
 const router=express.Router();
 
 router.post("/create",authMiddleware,createWorkspace);
 router.get("/getWorkspaces",authMiddleware,getWorkspace);
-
+router.delete("/deleteWorkspace/:workspaceId",authMiddleware,deleteWorkspace);
 export default router;
