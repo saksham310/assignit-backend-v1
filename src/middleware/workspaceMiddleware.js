13a5e6eafe6e00cbcfbe33b8bfafc6c1b085cab0
saksham.s <sakshams982@gmail.com>
2025-01-11 09:50:56 +0545

    #86er4czhf: Added auto generated Invite code for workspaces

--- src/middleware/workspaceMiddleware.js
+++ src/middleware/workspaceMiddleware.js
@@ -1,20 +1,23 @@
 import prisma from "../prismaClient.js";
 
 const workspaceMiddleware=async (req, res, next) => {
-    const {workspaceId} = req.params;
-    const w_id =parseInt(workspaceId);
-    const user=await prisma.workspace_User.findFirst({
-        where:{
-            workspace_id:w_id,
-            user_id:req.userId
+    try {
+        const {workspaceId} = req.params;
+        const w_id = parseInt(workspaceId);
+        const user = await prisma.workspace_User.findFirst({
+            where: {
+                workspace_id: w_id,
+                user_id: req.userId
+            }
+        });
+        if (!user) {
+            return res.status(404).json({message: 'Something went wrong'});
         }
-    });
-    if(!user){
-        return res.status(404).json({message:'Something went wrong'});
+        req.isOwner = user.role === "Owner";
+        req.w_id = w_id;
+        next();
     }
-    req.isOwner=user.role==="Owner";
-    req.w_id=w_id;
-    next();
-}
-
+catch(e){
+   console.error(e);
+}}
 export default workspaceMiddleware;
