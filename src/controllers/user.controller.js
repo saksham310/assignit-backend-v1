a96566efd343626318867926098a31f1464ac7a8
saksham.s <sakshams982@gmail.com>
2025-01-16 12:00:40 +0545

    updated the logic for user profile update

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -4,24 +4,39 @@ import bcrypt from "bcryptjs";
 
 export const updateUser = async (req, res) => {
     const {username, email, password,} = req.body;
-    const hashedPassword = bcrypt.hashSync(password, 10);
+    let updatedProfile;
     try {
         let imageUrl;
         if (req.file) {
             const fileBuffer = req.file.buffer;
             imageUrl=await uploadImage(fileBuffer);
         }
-        const updatedProfile=await prisma.user.update({
-            data:{
-                username,
-                email,
-                password:hashedPassword,
-                imageUrl,
-            },
-            where:{
-                id: req.userId,
-            }
-        })
+        if(password){
+            const hashedPassword = bcrypt.hashSync(password, 10);
+            updatedProfile=await prisma.user.update({
+                data:{
+                    username,
+                    email,
+                    password:hashedPassword,
+                    imageUrl,
+                },
+                where:{
+                    id: req.userId,
+                }
+            })
+        }else{
+            updatedProfile=await prisma.user.update({
+                data:{
+                    username,
+                    email,
+                    imageUrl,
+                },
+                where:{
+                    id: req.userId,
+                }
+            })
+        }
+
         return res.status(200).json(updatedProfile);
     } catch (error) {
         console.log(error);
