0c3dd57a1b98f1ee52a6e9270366658467639b74
saksham.s <sakshams982@gmail.com>
2025-01-17 06:03:25 +0545

    Added  a function to generate OTP code

---
+++ src/utils/otp.generator.js
@@ -0,0 +1,10 @@
+export const  generateOTP = () => {
+    let digits = '0123456789';
+    let OTP = '';
+    let len = digits.length
+    for (let i = 0; i < 4; i++) {
+        OTP += digits[Math.floor(Math.random() * len)];
+    }
+
+    return OTP;
+}
