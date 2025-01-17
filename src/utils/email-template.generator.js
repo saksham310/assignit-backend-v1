b327337f06037c274d1c310f9e488006afd804a9
saksham.s <sakshams982@gmail.com>
2025-01-17 06:04:26 +0545

    reset password mail template

---
+++ src/utils/email-template.generator.js
@@ -0,0 +1,19 @@
+export const generateResetPasswordEmail = (userName, otp) => {
+    const otpDigits = otp.split('').map(digit => {
+        return `<div style="display: inline-block; width: 40px; height: 40px; margin: 5px; border: 2px solid #4CAF50; border-radius: 5px; text-align: center; line-height: 40px; font-size: 1.5rem; font-weight: bold; color: #4CAF50;">${digit}</div>`;
+    }).join('');
+    return {
+        body: {
+            name: userName,
+            intro: `You have requested to reset your password.\nHere is your 4-digit OTP to reset your password:`,
+            table: {
+                data: [
+                    {
+                        'Your OTP': `<div style="text-align: center;">${otpDigits}</div>`
+                    }
+                ]
+            },
+            outro: 'If you did not request a password reset, please ignore this email or contact support for assistance.',
+        }
+    };
+};
