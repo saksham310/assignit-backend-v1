076df135b3403649dabe128ae897ec2c07aa2f5e
sakshams310 <sakshams982@gmail.com>
2025-05-19 20:28:30 +0545

    Updated socket api

--- src/server.js
+++ src/server.js
@@ -17,7 +17,7 @@ const server = http.createServer(app);
 
 export const io = new Server(server, {
     cors: {
-        origin: 'http://localhost:5173', // frontend URL
+        origin: 'https://assignit-frontend.vercel.app', // frontend URL
         methods: ['GET', 'POST', 'PUT', 'DELETE'],
         credentials: true,
     },
